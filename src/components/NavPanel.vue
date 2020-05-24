64c8195c84457c3dcf2cd91a56fcc8d632b25505
Cameron Moy <camoy@cs.umd.edu>
2020-05-24 12:02:42 -0400

    Cleanup nav

--- src/components/NavPanel.vue
+++ src/components/NavPanel.vue
@@ -10,36 +10,37 @@
     label="Packages"
     multiple
     />
-  <svg id="nav-svg" viewBox="0.5 -120.5 954 1320" style="font: 30px sans-serif">
+  <svg
+    viewBox="0.5 -120.5 954 1320"
+    style="font: 30px sans-serif">
     <g
       v-for="(node, k) in nodes"
-      class="list-item"
       :key="k"
-      :transform="node === root ? `translate(0,-120)` : `translate(${X(node.x0)},${Y(node.y0)})`"
+      :transform="node === root ? `translate(0,-120)` : `translate(${x(node.x0)},${y(node.y0)})`"
       :cursor="(node !== root || node.parent) ? 'pointer' : 'auto'"
       @click="zoom(node)"
       >
-      <title> {{ node.data.name }} ({{ FORMAT(node.value) }}) </title>
+      <title> {{ node.data.name }} ({{ commaFormat(node.value) }}) </title>
       <rect
         stroke="#fff"
-        :id="(node.leafUid = UID('leaf')).id"
-        :fill="COLOR(root, node)"
-        :width="node === root ? 954 : X(node.x1) - X(node.x0)"
-        :height="node === root ? 120 : Y(node.y1) - Y(node.y0)"
+        :id="setLeafUID(node)"
+        :fill="color(node)"
+        :width="node === root ? 954 : x(node.x1) - x(node.x0)"
+        :height="node === root ? 120 : y(node.y1) - y(node.y0)"
         />
-      <clipPath :id="(node.clipUid = UID('clip')).id">
+      <clipPath :id="setClipUID(node)">
         <use :xlink:href="node.leafUid.href" />
       </clipPath>
       <text
-        dominant-baseline="hanging"
+        dominant-baseline="hanging?"
         :clip-path="node.clipUid"
         :font-weight="node === root ? 'bold' : null"
         >
-        <tspan x="1em" y="1.1em">
+        <tspan x="1em" y="1.5em">
           {{ node.data.name }}
         </tspan>
-        <tspan x="1em" y="2.5em" fill-opacity="0.7" font-weight="normal">
-          {{ NUMERAL_FORMAT(node.value) }}
+        <tspan x="1em" y="3em" fill-opacity="0.7" font-weight="normal">
+          {{ commaFormat(node.value) }}
         </tspan>
       </text>
     </g>
@@ -79,8 +80,26 @@ Id.prototype.toString = function() {
     return "url(" + this.href + ")";
 }
 
-function COLOR(root, node) {
-    if (node === root) {
+//
+// Constant
+//
+const height = 1200;
+const width = 954;
+const LIMIT = 20;
+const TILE = d3.treemapSquarify
+
+//
+// Method
+//
+const x = d3.scaleLinear().rangeRound([0, width]);
+const y = d3.scaleLinear().rangeRound([0, height]);
+const plainFormat = (d) => numeral(d).format("0a");
+const commaFormat = d3.format(",d");
+const setLeafUID = node => (node.leafUid = UID("leaf")).id
+const setClipUID = node => (node.clipUid = UID("clip")).id
+
+function color(node) {
+    if (node === this.root) {
         return "#fff";
     } else if (node.children) {
         return "#ccc";
@@ -89,30 +108,30 @@ function COLOR(root, node) {
     }
 }
 
-//
-// TODO
-//
+function zoom(node) {
+    if (node === this.root && node.parent) {
+        x.domain([node.parent.x0, node.parent.x1]);
+        y.domain([node.parent.y0, node.parent.y1]);
+        setRoot.call(this, node.parent);
+    } else if (node.children) {
+        x.domain([node.x0, node.x1]);
+        y.domain([node.y0, node.y1]);
+        setRoot.call(this, node);
+    } else {
+        node.selected = !node.selected;
+        this.$store.commit("toggleFun", nodeFun(node));
+        this.$store.dispatch("queryTypes");
+        // HACK: Apparently Vue can't track set mutations.
+        this.$forceUpdate();
+    }
+}
 
-const height = 1200;
-const width = 954;
-const NUMERAL_FORMAT = (d) => numeral(d).format("0a");
-const FORMAT = d3.format(",d");
-const LIMIT = 20;
-const X = d3.scaleLinear().rangeRound([0, width]);
-const Y = d3.scaleLinear().rangeRound([0, height]);
+//
+// Treemap
+//
 
 // HACK: For when your language doesn't have value equality.
 const nodeFun = (node) => `${node.parent.data.name}☹${node.data.name}`
-
-function tile(node, x0, y0, x1, y1) {
-    d3.treemapBinary(node, 0, 0, width, height);
-    for (const child of node.children) {
-        child.x0 = x0 + child.x0 / width * (x1 - x0);
-        child.x1 = x0 + child.x1 / width * (x1 - x0);
-        child.y0 = y0 + child.y0 / height * (y1 - y0);
-        child.y1 = y0 + child.y1 / height * (y1 - y0);
-    }
-}
 
 function setRoot(root) {
     let nodes = root.children ? root.children.concat(root) : [];
@@ -151,30 +170,10 @@ function makeTree() {
         pkg.children = pkg.children.slice((this.page - 1) * LIMIT, this.page * LIMIT);
     }
     let data = { name: "packages", children: selectedChildren };
-
-    // Set root
     let hierarchy = d3.hierarchy(data).sum(d => d.value).sort((a, b) => b.value - a.value);
-    let root = d3.treemap().tile(tile)(hierarchy);
+    let root = d3.treemap().tile(TILE)(hierarchy);
     let newRoot = this.root ? findNewRoot.call(this, root) : root;
     setRoot.call(this, newRoot);
-}
-
-function zoom(node) {
-    if (node === this.root && node.parent) {
-        X.domain([node.parent.x0, node.parent.x1]);
-        Y.domain([node.parent.y0, node.parent.y1]);
-        setRoot.call(this, node.parent);
-    } else if (node.children) {
-        X.domain([node.x0, node.x1]);
-        Y.domain([node.y0, node.y1]);
-        setRoot.call(this, node);
-    } else {
-        node.selected = !node.selected;
-        this.$store.commit("toggleFun", nodeFun(node));
-        this.$store.dispatch("queryTypes");
-        // HACK: Apparently Vue can't track set mutations.
-        this.$forceUpdate();
-    }
 }
 
 //
@@ -185,16 +184,17 @@ export default {
     created() { this.$store.dispatch("queryPkgs"); },
     watch: { selectedPkgs: makeTree, page: makeTree },
     computed: mapState(["pkgs", "funsShown"]),
-    methods: { zoom },
+    methods: {
+        x,
+        y,
+        setLeafUID,
+        setClipUID,
+        plainFormat,
+        commaFormat,
+        color,
+        zoom,
+    },
     data: () => ({
-        LIMIT,
-        X: X,
-        Y: Y,
-        COLOR: COLOR,
-        UID: UID,
-        NUMERAL_FORMAT: NUMERAL_FORMAT,
-        FORMAT: FORMAT,
-
         root: false,
         nodes: [],
         page: 1,
