c128490c9946110a2c48cfd0004e7c11e0b5810c
Cameron Moy <camoy@cs.umd.edu>
2020-05-28 22:37:47 -0400

    Cleanup app and about

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <v-app>
-    <v-content>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col cols="10" style="height: 90vh">
-            <TabView />
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
-  </v-app>
+<v-app>
+  <v-content>
+    <v-container class="fill-height" fluid>
+      <v-row justify="center">
+        <v-col id="app-vcol" cols="10">
+          <TabView />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</v-app>
 </template>
+
+<style>
+  #app-vcol {
+    height: 90vh;
+  }
+</style>
 
 <script>
 import TabView from "./components/TabView";
