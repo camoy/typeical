c8604440834a764647d003849753e6791268c371
Cameron Moy <camoy@cs.umd.edu>
2020-05-28 23:52:28 -0400

    Progress on nav cleanup

--- src/components/NavPanel.vue
+++ src/components/NavPanel.vue
@@ -1,60 +1,83 @@
 <template>
 <v-container>
+  <!-- Package selection -->
   <v-autocomplete
     v-model="selectedPkgs"
-    :items="$store.getters.pkgNames"
     outlined
     chips
     small-chips
     deletable-chips
     label="Packages"
     multiple
+    :items="$store.getters.pkgNames"
     />
-  <svg
-    viewBox="0.5 -120.5 954 1320"
-    style="font: 30px sans-serif">
+
+  <!-- Treemap -->
+  <svg id="nav-svg" viewBox="0.5 -120.5 954 1320">
+    <!-- No Data -->
     <g v-if="nodes.length === 0">
-      <rect fill="none" stroke-width="2" rx="5" stroke="#666" x="25" y="25" width="904" height="1150" />
-      <text text-anchor="middle" x="477" y="660"> No Packages Selected </text>
+      <rect
+        fill="none"
+        stroke-width="2"
+        rx="5"
+        stroke="#666"
+        x="25"
+        y="25"
+        width="904"
+        height="1150"
+        />
+      <text text-anchor="middle" x="477" y="660">
+        No Packages Selected
+      </text>
     </g>
-    <g
-      v-for="(node, k) in nodes"
-      :key="k"
-      :transform="node === root ? `translate(0,-120)` : `translate(${x(node.x0)},${y(node.y0)})`"
-      :cursor="(node !== root || node.parent) ? 'pointer' : 'auto'"
-      @click="zoom(node)"
-      >
-      <title> {{ node.data.name }} ({{ commaFormat(node.value) }}) </title>
+
+    <!-- Data -->
+    <g v-for="(node, k) in nodes"
+       :key="k"
+       :transform="node === root ? `translate(0,-120)` : `translate(${x(node.x0)},${y(node.y0)})`"
+       :cursor="(node !== root || node.parent) ? 'pointer' : 'auto'"
+       @click="select(node)"
+       >
+      <title> {{ node.data.name }} ({{ exactFormat(node.value) }}) </title>
       <rect
         stroke="#fff"
         :id="setLeafUID(node)"
-        :fill="color(node)"
         :width="node === root ? 954 : x(node.x1) - x(node.x0)"
         :height="node === root ? 120 : y(node.y1) - y(node.y0)"
+        :fill="color(node)"
         />
       <clipPath :id="setClipUID(node)">
         <use :xlink:href="node.leafUid.href" />
       </clipPath>
       <text
-        dominant-baseline="hanging?"
+        dominant-baseline="hanging"
         :clip-path="node.clipUid"
         :font-weight="node === root ? 'bold' : null"
         >
-        <tspan x="1em" y="1.5em">
+        <tspan x="1em" y="1em">
           {{ node.data.name }}
         </tspan>
-        <tspan x="1em" y="3em" fill-opacity="0.7" font-weight="normal">
-          {{ nodeText(node) }}
+        <tspan x="1em" y="2.25em" fill-opacity="0.7" font-weight="normal">
+          {{ exactFormat(node.data.labelValue || node.data.value) }}
         </tspan>
       </text>
     </g>
   </svg>
+
+  <!-- Pagination -->
   <v-pagination
     v-if="root.parent"
     v-model="page"
-    :length="root.data.pages || 1" />
+    :length="root.data.pages || 1"
+    />
 </v-container>
 </template>
+
+<style>
+#nav-svg {
+    font: 30px sans-serif;
+}
+</style>
 
 <script>
 //
@@ -62,16 +85,65 @@
 //
 import * as d3 from "d3";
 import { mapState } from "vuex";
-import numeral from "numeral";
 const _ = require("lodash");
 
 //
-// UID
+// Constants
+//
+const ROOT_COLOR = "#fff";
+const SELECTED_COLOR = "#da4f81";
+const DEFAULT_COLOR = "#ccc";
+const TILING = d3.treemapSquarify;
+const LIMIT = 20;
+const WIDTH = 954;
+const HEIGHT = 1200;
+
+//
+// Methods
 //
 
-var count = 0;
+// Node → String
+// Sets and returns a node's leaf UID.
+const setLeafUID = (node) => (node.leafUid = UId("leaf")).id;
 
-function UID(name) {
+// Node → String
+// Sets and returns a node's clip UID.
+const setClipUID = (node) => (node.clipUid = UId("clip")).id;
+
+// Number → String
+// Returns the given number exactly, but readable (usually comma separated).
+const exactFormat = (x) => x.toLocaleString();
+
+// Node → String
+// Returns the color of the given treemap node. If it's the root, then
+// the root color. If selected, then use the selection color. Otherwise
+// use the default.
+function color(node) {
+    if (node === this.root) return ROOT_COLOR;
+    if (this.funsShown.includes(nodeFun(node))) return SELECTED_COLOR;
+    return DEFAULT_COLOR;
+}
+
+// Node → Any
+// If the node is the root, move up a level in the treemap. If the node has
+// children, move down a level in the treemap. Otherwise, this is a leaf
+// node, so we select the function at the node.
+function select(node) {
+    if (node === this.root && node.parent) return this.root = node.parent;
+    if (node.children) return this.root = node;
+
+    this.$store.commit("toggleFun", nodeFun(node));
+    this.$store.dispatch("queryTypes");
+}
+
+//
+// UId Functions
+// Source: https://github.com/observablehq/stdlib
+//
+
+let count = 0;
+
+function UId(name) {
     return new Id("O-" + (name == null ? "" : name + "-") + ++count);
 }
 
@@ -85,82 +157,24 @@ Id.prototype.toString = function() {
 }
 
 //
-// Constant
-//
-const height = 1200;
-const width = 954;
-const LIMIT = 20;
-
-//
-// Method
-//
-const x = d3.scaleLinear().rangeRound([0, width]);
-const y = d3.scaleLinear().rangeRound([0, height]);
-const plainFormat = (d) => numeral(d).format("0a");
-const commaFormat = d3.format(",d");
-const setLeafUID = node => (node.leafUid = UID("leaf")).id
-const setClipUID = node => (node.clipUid = UID("clip")).id
-
-function selected(node) {
-    return this.funsShown.includes(nodeFun(node));
-}
-
-function color(node) {
-    if (node === this.root) {
-        return "#fff";
-    } else if (node.children) {
-        return "#ccc";
-    } else {
-        return selected.call(this, node) ? "#da4f81" : "#ccc";
-    }
-}
-
-function zoom(node) {
-    if (node === this.root && node.parent) {
-        setRoot.call(this, node.parent);
-    } else if (node.children) {
-        setRoot.call(this, node);
-    } else {
-        this.$store.commit("toggleFun", nodeFun(node));
-        this.$store.dispatch("queryTypes");
-    }
-}
-
-function nodeText(node) {
-    if (!node.data.realValue) {
-        return commaFormat(node.value);
-    } else {
-        return commaFormat(node.data.realValue);
-    }
-}
-
-//
 // Treemap
+// Source: https://observablehq.com/@d3/zoomable-treemap
 //
 
 // HACK: For when your language doesn't have value equality.
 const nodeFun = (node) => `${node.parent.data.name}☹${node.data.name}`
 
+// Node Number Number Number Number → Any
+// Sets the dimensions of the children of the current node based on the
+// treemap layout algorithm.
 function tile(node, x0, y0, x1, y1) {
-    d3.treemapSquarify(node, 0, 0, width, height);
+    TILING(node, 0, 0, WIDTH, HEIGHT);
     for (const child of node.children) {
-        child.x0 = x0 + child.x0 / width * (x1 - x0);
-        child.x1 = x0 + child.x1 / width * (x1 - x0);
-        child.y0 = y0 + child.y0 / height * (y1 - y0);
-        child.y1 = y0 + child.y1 / height * (y1 - y0);
+        child.x0 = x0 + child.x0 / WIDTH * (x1 - x0);
+        child.x1 = x0 + child.x1 / WIDTH * (x1 - x0);
+        child.y0 = y0 + child.y0 / HEIGHT * (y1 - y0);
+        child.y1 = y0 + child.y1 / HEIGHT * (y1 - y0);
     }
-}
-
-function updateDomains() {
-    let root = this.root;
-    x.domain([root.x0, root.x1]);
-    y.domain([root.y0, root.y1]);
-}
-
-function setRoot(root) {
-    let nodes = root.children ? root.children.concat(root) : [];
-    this.root = root;
-    this.nodes = nodes;
 }
 
 function findNewRoot(root) {
@@ -187,20 +201,20 @@ function makeTree() {
         _.cloneDeep(this.pkgs.children)
         .filter(x => selectedPkgs.includes(x.name));
     for (let pkg of selectedChildren) {
-        pkg.realValue = pkg.children.reduce((acc, x) => acc + x.value, 0);
+        pkg.labelValue = pkg.children.reduce((acc, x) => acc + x.value, 0);
         pkg.pages = Math.ceil(pkg.children.length / LIMIT);
         pkg.children = pkg.children.slice((this.page - 1) * LIMIT, this.page * LIMIT);
     }
-    let realValue = selectedChildren.reduce((acc, x) => acc + x.realValue, 0);
-    let data = { name: "packages", children: selectedChildren, realValue };
+    let labelValue = selectedChildren.reduce((acc, x) => acc + x.labelValue, 0);
+    let data = { name: "packages", children: selectedChildren, labelValue };
     let hierarchy = d3.hierarchy(data).sum(d => d.value).sort((a, b) => b.value - a.value);
     let root = d3.treemap().tile(tile)(hierarchy);
     let newRoot = this.root ? findNewRoot.call(this, root) : root;
-    setRoot.call(this, newRoot);
+    this.root = newRoot;
 }
 
 //
-// Export
+// Exports
 //
 export default {
     name: "NavPanel",
@@ -212,24 +226,36 @@ export default {
             makeTree.call(this);
         },
         pkgs: makeTree,
-        page: makeTree,
-        root: updateDomains
+        page: makeTree
     },
-    computed: mapState(["pkgs", "funsShown"]),
+    computed:{
+        nodes() {
+            let root = this.root;
+            return root && root.children ? root.children.concat(root) : [];
+        },
+        x() {
+            return d3
+                .scaleLinear()
+                .rangeRound([0, WIDTH])
+                .domain([this.root.x0, this.root.x1]);
+        },
+        y() {
+            return d3
+                .scaleLinear()
+                .rangeRound([0, HEIGHT])
+                .domain([this.root.y0, this.root.y1]);
+        },
+        ...mapState(["pkgs", "funsShown"])
+    },
     methods: {
-        x,
-        y,
         setLeafUID,
         setClipUID,
-        plainFormat,
-        commaFormat,
+        exactFormat,
         color,
-        zoom,
-        nodeText
+        select
     },
     data: () => ({
         root: false,
-        nodes: [],
         page: 1,
         selectedPkgs: []
     })
