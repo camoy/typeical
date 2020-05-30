8b8e0b13a3b24b455ff705abe2e3ffcedf7e15a7
Julia Belyakova <julbinb@gmail.com>
2020-05-30 03:23:20 +0200

    baseurl depends on env

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const beforeConfig = require('./src/server/api')
 
 module.exports = {
+    publicPath: process.env.BASE_MODE === 'server'
+	? '/rtypes-viz-new/'
+        : '/',
   devServer: { before: beforeConfig },
   transpileDependencies: ["vuetify"]
 };
